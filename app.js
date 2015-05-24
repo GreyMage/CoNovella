51aea3ab2fb73413501435231b8fc7959edb3dd4
Cades <bulzeeb@gmail.com>
2015-05-24 02:40:37 -0700

    awesome shit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var _package = require('./package.json');
 
 var deferred = require('deferred');
+var Datastore = require('nedb');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -16,54 +17,149 @@ app.get('/', function (req, res) {
   res.render('index', { name: _package.name, INDEV:INDEV});
 }); 
 
-var Story = function(obj){
-	for(var i in obj){
-		this[i] = obj[i];
+// Configure Library
+var Library = new Datastore({ filename: 'db/library.nedb', autoload: true });
+Library.persistence.setAutocompactionInterval(1000*60*10); // Ten minutes
+
+// Configure Librarian 
+var Librarian = {};
+Librarian.collection = [];
+Librarian.speed = 3000;
+Librarian.manage = function(story){
+	if(!story) return;
+	if(!~Librarian.collection.indexOf(story)){
+		Librarian.collection.push(story);
 	}
+	return story;
 };
-Story.prototype = {
-	id:false,
-	title:"New Story",
-	synopsis:"No synopsis",
-	verbiage:"",	
-	append:function(text){
-		this.verbiage += " "+text.trim();
-		io.to(this.id).emit('append',{
-			storyId:this.id,
-			verbiage:text.trim()
+Librarian.tick = function(){
+
+	var d = new deferred();	
+	var s = Librarian.manage(Librarian.collection.shift());
+	
+	d.promise.finally(function(){
+		setTimeout(function(){
+			Librarian.tick();
+		},Librarian.speed);
+	});
+	
+	if(!s) { d.resolve(); return; }
+	
+	if(!s.dirty){
+		d.resolve();
+	} else {
+		var p = s.save()
+		p.catch(function(err){
+			console.log("something went wrong...",err);
+		});
+		p.finally(function(){
+			d.resolve(s);
 		});
 	}
+	
 }
-
-var stories = []; 
-
-stories.push(new Story({
-	id:"banana",
-	title:"Test",
-	verbiage:"Test"
-}));
-
-stories.push(new Story({
-	id:"e5f62ncvo3",
-	title:"Test2",
-	verbiage:"THIS IS A LONGER"
-}));
-
-stories.push(new Story({
-	id:"dwdawfg32g2",
-	title:"Much longer test name to check how it reacts.",
-	verbiage:""
-}));
-
-stories.push(new Story({
-	id:"dwadawfg4vres",
-	title:"A name that is simply too long to be properly allowed, but is good for seeing what happens to the frontend.",
-	verbiage:""
-}));
-
-var getTopStories = function(){
-	return stories;
+Librarian.getAllStories = function(){
+	var ensureInit = new deferred();
+	var d = new deferred();
+	Librarian.init().done(function(){
+		d.resolve(Librarian.collection); //TODO: Maybe make this a copy?
+	});
+	return d.promise;
 };
+Librarian.getStory = function(id){
+	var d = new deferred();
+	Librarian.init().done(function(){
+		for(var i=0;i<Librarian.collection.length;i++){
+			if(Librarian.collection[i]._id == id) d.resolve(Librarian.collection[i]);
+		}
+		d.reject();
+	});
+	return d.promise;
+};
+Librarian.getTopStories = function(id){
+	var d = new deferred();
+	Librarian.init().done(function(){
+		var top = [], max = 5
+		for(var i=0;i<Librarian.collection.length;i++){
+			if(top.length<max) top.push(Librarian.collection[i]);
+		}
+		d.resolve(top);
+	});
+	return d.promise;
+};
+Librarian.init = function(){
+	var d = new deferred();
+	if(Librarian.inited) {
+		d.resolve();
+		return d.promise;
+	}
+	Librarian.inited = true;
+	Library.find({}, function (err, docs) {
+		var inflated = [];
+		for(var i=0;i<docs.length;i++){
+			var s = new Story(docs[i]);
+			Librarian.manage(s);
+			inflated.push(s);
+		}
+		d.resolve(inflated);
+	});
+	Librarian.tick();
+	return d.promise;
+};
+
+// Stories use the library Automatically.
+var Story = function(obj){
+	this.inflate(obj);
+};
+
+Story.prototype = {
+	title:"New Story",
+	synopsis:"No synopsis",
+	verbiage:"",
+	updated:0,
+	deflate:function(){
+		return JSON.stringify(this);
+	},
+	inflate:function(data){
+		if(typeof data != "object") var data = JSON.parse(data);
+		for(var i in data){
+			this[i] = data[i];
+		}
+	},
+	save:function(){
+		var self = this;
+		var d = new deferred();
+		if(this._id){
+			Library.update({_id:this._id}, this, {},function (err, numReplaced) {
+				if(err) d.reject(err);
+				d.resolve();
+			});
+		} else {
+			Library.insert(this, function (err, newDoc) {   // Callback is optional
+				if(err) d.reject(err);
+				self.inflate(newDoc);
+				d.resolve();
+			});
+		}
+		d.promise.done(function(){
+			self.dirty=false;
+		});
+		return d.promise;
+	},
+	append:function(text){
+		this.verbiage += " "+text.trim();
+		this.dirty=true;
+		this.updated=new Date().getTime();
+		//console.log(this.channel(),"blah");
+		io.to(this.channel()).emit('append',{
+			_id:this._id,
+			verbiage:text.trim()
+		});
+	},
+	channel:function(){
+		return "STORY-"+this._id;
+	}
+}
 
 var noise = function(story){
 	
@@ -75,22 +171,64 @@ var noise = function(story){
 	
 	setTimeout(function(){
 		noise(story);
-	},300+(Math.random()*3217))
+	},300+(Math.random()*7217));
+	
 }
-for(var x in stories){
-	console.log("noise on",stories[x].id)
-	noise(stories[x]);
+
+// Do stuff
+Librarian.getAllStories().done(function(stories){
+	for(var x in stories) {
+		console.log("noise on",stories[x].title)
+		noise(stories[x]);
+	}
+});
+
+var leaveAllStoryRooms = function(socket){
+	var d = new deferred();
+	var count = 0;
+	socket.rooms.forEach(function(room){
+		if(!!room.match(/^STORY-/)){
+			count++
+			socket.leave(room,function(){
+				console.log("socket left channel",room);
+				if(!--count) d.resolve();
+			});
+			
+		}
+	});
+	
+	if(!count) d.resolve();
+	
+	return d.promise;
 }
 
 io.on('connection', function (socket) {
+
 	socket.on('getTopStories', function () {
-		var sto = getTopStories();
-		sto.forEach(function(story){
-			socket.join(story.id);
-			console.log("socket joining channel",story.id);
-		});
-		socket.emit("recvTopStories",getTopStories());
+		leaveAllStoryRooms(socket).done(function(){
+			Librarian.getTopStories().done(function(topStories){
+				topStories.forEach(function(story){
+					socket.join(story.channel());
+					console.log("socket joining channel",story.channel());
+				});
+				socket.emit("recvTopStories",topStories);
+			});
+		})
+		
 	});
+	
+	socket.on('spectate',function(data){
+		leaveAllStoryRooms(socket).done(function(){
+			Librarian.getStory(data._id).done(function(story){
+				socket.join(story.channel());
+				console.log("socket joining channel",story.channel());
+				console.log("now in rooms",socket.rooms);
+				socket.emit("recvSpectate",story);
+			});
+		});
+		
+	});
+	
 });
 
 server.listen(3000);
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,6 +1,7 @@
 (function(){
 	// WELCOME TO MY NAMESPACE MUAHAHA.	
 	// Few fun bits
+	
 	var deferred = function(){
         var resolved = false;
         var rejected = false;
@@ -75,7 +76,95 @@
 		i.classList.add("fa-"+name);
 		return i;
 	};
+	function fadeIn(el) {
+		var d = new deferred();
+		el.style.opacity = 0;
+
+		var last = +new Date();
+		var tick = function() {
+			el.style.opacity = +el.style.opacity + (new Date() - last) / 400;
+			last = +new Date();
+
+			if (+el.style.opacity < 1) {
+				if (window.requestAnimationFrame)
+					requestAnimationFrame(tick);
+				else 
+					setTimeout(tick, 16);
+			} else {
+				d.resolve();
+			}
+		};
+
+		tick();
+		return d.promise();
+	}
+	function fadeOut(el) {
+		var d = new deferred();
+		el.style.opacity = 1;
+
+		var last = +new Date();
+		var tick = function() {
+			el.style.opacity = +el.style.opacity - (new Date() - last) / 400;
+			last = +new Date();
+
+			if (+el.style.opacity > 0) {
+				if (window.requestAnimationFrame)
+					requestAnimationFrame(tick);
+				else 
+					setTimeout(tick, 16);
+			} else {
+				d.resolve();
+			}
+		};
+
+		tick();
+		return d.promise();
+	}
+
+	var getFragObj = function(){
+		var base = window.location.hash.match(/^#(.*)/);
+		if(!base) return {};
+		var bits = base[1].split("&");
+		var out = {};
+		bits.forEach(function(elem){
+		  var splat = elem.split("=");
+		  out[splat[0]] = true;
+		  if(splat.length > 1) out[splat[0]] = splat[1];
+		});
+		return out;
+	};
 	
+	var setFragObj = function(obj){
+		if(!obj && window.location.hash.length > 0){
+			console.log("pushing history state to /");
+			history.pushState({}, "", "/");
+			return;
+		}
+		var frag = "#";
+		var temp = [];
+		for(var key in obj){
+			var value = obj[key];
+			temp.push(key+"="+value);
+		}
+		frag += temp.join("&");
+		
+		if(frag == "#") frag = "";
+		
+		if(frag != window.location.hash){
+			console.log(window.location.hash,"!=",frag);
+			console.log("pushing history state to ",frag);
+			history.pushState({}, "", frag);
+		}
+	};
+	
+	var modFragObj = function(delta){
+		var x = getFragObj;
+		for(var i in delta){
+			x[i] = delta[i];
+		}
+		setFragObj(x);
+	};
+
 	// Init storage
 	var store = new storage();
 	// Init socket.io
@@ -106,34 +195,124 @@
 	
 	var getTopStories = function(){
 		var def = new deferred();
-		var handleTopStories = function(obj){
+		socket.once('recvTopStories', function(obj){
 			obj = obj || [];
-			socket.removeListener("recvTopStories",handleTopStories);
 			def.resolve(obj);
-			console.log(obj);
-		};
-		socket.on('recvTopStories', handleTopStories);
+		});
 		socket.emit('getTopStories');
 		return def.promise();
 	};
+	var showScreen = function(el){
 	
-	var showTopStories = function(){ 
-	
-		var topList = document.getElementById('topList');
+		var d = new deferred();
 		
+		socket.removeAllListeners("append"); // unhook from any repeat screen events here.
+		
+		var screens = document.getElementsByClassName('screen'); 
+		var waitFor = 0;
+		var vanish = function(el){
+			return function(){
+				el.classList.remove("show");
+				if(!--waitFor) d.resolve();
+			};
+		};
+		for(var i=0;i<screens.length;i++){
+			if(screens[i].classList.contains("show")){
+				waitFor++;
+				fadeOut(screens[i]).done(vanish(screens[i]));
+			}
+		}
+		
+		if(!waitFor) d.resolve();
+		
+		d.done(function(){
+			el.classList.add("show");
+			fadeIn(el);
+		});
+		
+	};
+	var showSpecificStory = function(data){
+		var fullPage = document.getElementById('fullPage');
+		showScreen(fullPage);
+		
+		modFragObj({storyid:data._id});
+		
+		var verbiage = fullPage.getElementsByClassName("verbiage")[0];
+		verbiage.innerHTML = data.verbiage;
+		
+		if(!fullPage.getAttribute("data-scroll-event")){
+			var scrollVerbiage = function(e){
+			
+				var direction = (e.deltaY>0)?1:-1;
+				var speed = 50;
+				
+				var c = parseInt(verbiage.style.bottom,10) || 0;
+				c += speed*direction;
+				var _c = -1*c;
+				
+				// clamp
+				if(c>0)c=0;
+				if(_c > verbiage.clientHeight) c = -1 * verbiage.clientHeight;
+				
+				verbiage.style.bottom = c+"px";
+				
+				console.log(c,verbiage.clientHeight);
+				return false;
+				
+			};
+			
+			fullPage.addEventListener("wheel",scrollVerbiage);
+			fullPage.setAttribute("data-scroll-event",1);
+		}
+		
+		var back = fullPage.getElementsByClassName("back")[0];
+		
+		if(!back.getAttribute("data-back-event")){
+		
+			console.log("setting callback");
+			var goBack = function(){
+				showTopStories();
+			};
+			back.addEventListener("click",goBack);
+			back.setAttribute("data-back-event",1);
+			
+		}
+		
+		
+		var handleMainUpdate = function(obj){
+			if(obj._id == data._id)
+				verbiage.innerHTML += " "+obj.verbiage;
+			else 
+				console.log("bad update",obj);
+		};
+		// CONTINUE HERE 
+		socket.on('append', handleMainUpdate);
+		
+	};
+	var showTopStories = function(){ 
+		console.log("showTopStories");
+		var topList = document.getElementById('topList');
+		showScreen(topList);
+		setFragObj(false);
+		
+		// Clean it out.
+		topList.innerHTML = "";
 		var loadTopStory = function(story){
 			
-			// BE AWARE! 
+			// BE AWARE!  
 			/*
 				We need to be able to update these with live edits from the server.
 				When we're loading all the stories the server will attach us to the 5 stories
 				update events, So we need to handle them, but after we enter one legit it will remove us from those. 
-			*/
+			*/			
 			
 			// Create Clickable story here
 			var storyDiv = document.createElement('div');
 			storyDiv.classList.add("story");
-			storyDiv.setAttribute("data-story-id",story.id);
+			storyDiv.setAttribute("data-story-id",story._id);
+			storyDiv.addEventListener("click",function(){
+				spectateStory(story._id);
+			});
 			
 			// This contains the live-updating verbiage of the story.
 			var verbiage = document.createElement('div');
@@ -166,8 +345,9 @@
 			
 			var domstories = topList.getElementsByClassName('story');
 			var handleTopUpdate = function(obj){
+				
 				for(var i=0;i<domstories.length;i++){
-					if(domstories[i].getAttribute("data-story-id") == obj.storyId){
+					if(domstories[i].getAttribute("data-story-id") == obj._id){
 						var c = domstories[i].getElementsByClassName('verbiage')[0];
 						c.innerHTML += " "+obj.verbiage;
 					}
@@ -193,7 +373,6 @@
 			
 			for(var i=0;i<dismissButtons.length;i++){
 				var el = dismissButtons[i];
-				console.log("attaching to",el);
 				el.addEventListener("click",dismiss);
 			}
 		} else {
@@ -202,9 +381,33 @@
 		return def.promise();
 	};
 	
+	var spectateStory = function(id){
+		var def = new deferred();
+		
+		socket.once('recvSpectate',function(data){
+			showSpecificStory(data);
+		});
+		socket.emit('spectate',{_id:id});
+		
+		return def.promise();
+	};
+	
+	var handleURL = function(){
+		// Check for frag, go direct or go home.
+		var frag = getFragObj();
+		if(frag.storyid){
+			spectateStory(frag.storyid);
+		} else {
+			showTopStories();
+		}
+	};
+	
+	window.addEventListener("popstate",handleURL);
+	
 	var initHook = initSocketIo();
 	initHook.done(function(){regEvents();});
-	initHook.done(function(){showTopStories();});
+	initHook.done(handleURL);
 	initHook.done(function(){handleWelcomeMsg();});
+	initHook.done(function(){});
   
 })(); 
